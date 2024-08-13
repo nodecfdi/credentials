9b28283b09f0a5a420f28302947b7f41b4d2c329
Fernando Isidro Luna <luffynando@gmail.com>
2024-08-13 15:03:19 -0600

    chore: :construction: prepare exports for release version

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from '#src/internal/key_type';
 export * from '#src/internal/sat_type';
 export { default as PemExtractor } from '#src/pem_extractor';
 export { default as SerialNumber } from '#src/serial_number';
+export * from '#src/utils/utilities';
